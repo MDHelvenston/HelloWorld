9abe63809f7314f46f9583bd77d80a4c70dbe1db
MDHelvenston <Xombor@hotmail.com>
2016-02-02 10:13:36 -0500

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,8 @@
+public class Hello 
+{
+    public static void main(String[] args)
+    {
+        System.out.println("Salutations Sphere!");
+        //The code above is what forces the computer into doing what you want
+    }
+}
